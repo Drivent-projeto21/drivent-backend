26462df3eb8f2889128542971820029e8216ab70
luiza-paloschi <luizapaloschi@gmail.com>
2023-05-16 15:50:27 -0300

    refactor: seed for tyckettypes and hotels

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -17,6 +17,23 @@ async function main() {
   }
 
   console.log({ event });
+
+  await prisma.ticketType.deleteMany();
+  await prisma.ticketType.createMany({ data: ticketTypes });
+
+  await prisma.hotel.deleteMany();
+  await prisma.hotel.createMany({ data: hotels });
+
+  const createdHotels = await prisma.hotel.findMany();
+
+  await prisma.room.deleteMany();
+  const rooms = [
+    {name: '101', capacity: 1, hotelId: createdHotels[0].id},
+    {name: '102', capacity: 2, hotelId: createdHotels[0].id},
+    {name: '101', capacity: 2, hotelId: createdHotels[1].id},
+    {name: '102', capacity: 3, hotelId: createdHotels[1].id},
+  ];
+  await prisma.room.createMany({ data: rooms });
 }
 
 main()
@@ -27,3 +44,35 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
+
+  const ticketTypes = [
+    {
+      name: 'Online',
+      price: 100,
+      isRemote: true,
+      includesHotel: false
+    },
+    {
+      name: 'Presencial',
+      price: 250,
+      isRemote: false,
+      includesHotel: false
+    },
+    {
+      name: 'Presencial',
+      price: 600,
+      isRemote: false,
+      includesHotel: true
+    }
+  ];
+
+  const hotels = [
+    {
+      name:'Driven World',
+      image: 'https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg'
+    },
+    {
+      name:'Driven Palace',
+      image: 'https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_871,h_581/https://blog.hotelpontaverde.com.br/wp-content/uploads/2019/09/Resort-ou-Hotel-Hotel-Ponta-Verde-France%CC%82s.png'
+    },
+  ];
