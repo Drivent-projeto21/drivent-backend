0b7d4f7261ca37875d7726fdfb7d9ca5815344c6
luiza-paloschi <luizapaloschi@gmail.com>
2023-05-17 20:25:41 -0300

    refactor: cached event info

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,19 @@
+import { Event } from '@prisma/client';
 import { prisma } from '@/config';
+import { redis } from '@/config/redis';
 
 async function findFirst() {
-  return prisma.event.findFirst();
+  const cachedEvent = await redis.get('event');
+
+  if (cachedEvent) {
+    const event: Event = JSON.parse(cachedEvent);
+    return event;
+  }
+
+  const event = await prisma.event.findFirst();
+  redis.setEx('event', 43200, JSON.stringify(event));
+
+  return event;
 }
 
 const eventRepository = {
